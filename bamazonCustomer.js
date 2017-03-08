78281bf853880451ab371ece6d0f81bb0b73e822
Hannah King <hmking.nc@gmail.com>
2017-03-07 20:25:20 -0500

    Working App, needs comments

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,120 @@
+//Dependencies
+//-----------------------------------------
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+//-----------------------------------------
+
+//Create mySql connection
+//-----------------------------------------
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 8889,
+    user: 'root',
+    password: 'hking5484',
+    database: 'bamazon_DB'
+});
+//-----------------------------------------
+
+//Open mySql Connection 
+//----------------------------------------------------------
+connection.connect(function(err){
+    if (err) throw err;
+    console.log("Connected as ID: " + connection.threadId);
+    run();
+});
+//----------------------------------------------------------
+
+//Function to run the app
+//--------------------------------------------------------------------
+var run = function(){
+    //Query All from products Table
+    //--------------------------------------------------------------------
+    connection.query('SELECT * FROM products WHERE inStock=1', function(err, res){
+        if (err) throw err  
+        //Fxn that Builds formatted product information for each item to display prior to prompt   
+        //--------------------------------------------------------------------
+        var choiceArr = function(val) {
+            console.log("choiceArr");
+            var choiceArr = [];
+            for (var i = 0; i < res.length; i++) {
+                var item = `ID: ${res[i].item_id} | Product: ${res[i].product_name} | Price: $${res[i].price}`
+                choiceArr.push(item);
+                //.product_name
+            };
+            return choiceArr;
+        };
+        //--------------------------------------------------------------------
+        //Fxn that Displays the formatted information before prompt
+        //--------------------------------------------------------------------
+        var showProducts = function() {
+            choiceArr = choiceArr();
+            for (var i = 0; i <  choiceArr.length; i++) {
+                console.log(choiceArr[i]);
+            };
+        };
+        //--------------------------------------------------------------------
+        showProducts();
+        //User form to capture and return an answer object
+        //--------------------------------------------------------------------
+        inquirer.prompt([
+            {
+                name: "item_choice",
+                type:"input",
+                message: "Please input the ID of the product you want to purchase.",
+            },
+            {
+                name: "itemqty",
+                type: "input",
+                message:"How many of those would you like to buy?"
+        }]).then(function(answerObj){ 
+            //logic for user input
+            var userInput = {
+                "item_id": answerObj.item_choice,
+                "req_quantity": answerObj.itemqty
+            };
+            var submitInput = function(){
+                connection.query("SELECT * FROM products WHERE item_id=" + answerObj.item_choice, function(err, res){
+                    //array selector necessary
+                    if(res[0].stock_quantity>=answerObj.itemqty){          
+                        var inventoryCount = function (){
+                            connection.query('SELECT * FROM products WHERE stock_quantity=0', function(err, res){
+                                if (err) throw err;
+                                for(var i=0; i < res.length; i++) {
+                                    if(res[i].stock_quantity === 0){
+                                        connection.query("UPDATE products WHERE stock_quantity=0 SET inStock=false");
+                                    };
+                                };
+                            });
+                            console.log("---------------------------------------");                                    
+                        };
+                        var updateQty = function(databaseStock) {
+                            var adjustedStock = parseInt(databaseStock) - answerObj.itemqty;
+                            connection.query("UPDATE products SET stock_quantity=" + adjustedStock + " WHERE item_id=" + answerObj.item_choice, function(err){
+                                if (err) throw err;
+                                console.log(`Stock Updated. ID: ${answerObj.item_choice}`)                                    
+                                console.log("---------------------------------------");                                    
+                            });
+                            inventoryCount();
+                            
+                        }
+
+                        updateQty(res[0].stock_quantity);
+                    } else { 
+                    console.log("Sorry. Only " + res[0].stock_quantity + " in stock.");
+                    }  
+                });
+            };
+            submitInput();
+        }).then(function(){ 
+            setTimeout(function(){
+                console.log("Ending Connection");
+                connection.end();
+            }, 1000)
+        });
+        //--------------------------------------------------------------------
+    });
+    //--------------------------------------------------------------------
+};
+//--------------------------------------------------------------------
+
+
--- bamazonSeeds.sql
+++ bamazonSeeds.sql
@@ -1,14 +1,14 @@
 CREATE DATABASE bamazon_DB;
 USE bamazon_DB;
 CREATE TABLE products (
-    item_id INT(11) AUTO_INCREMENT NOT NULL,
+    item_id INT(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
     product_name VARCHAR(30) NOT NULL,
     department_name VARCHAR(30) DEFAULT 'Misc',
     price DECIMAL(10,2) NOT NULL,
     stock_quantity INT(11) NOT NULL
-)
+);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity) {
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES
     ("Eloquent JS", "Books", 17.95, 75),
     ("Cherry Chapstick", "Beauty Products", 2.50, 1000),
     ("Clip Board", "Suppies", 7.50, 1000),
@@ -18,5 +18,13 @@ INSERT INTO products (product_name, department_name, price, stock_quantity) {
     ("JavaScript and JQuery: Interactive Front-End", "Books", 27.51, 1000),
     ("Dog Food", "Pet Products", 42.50, 200),
     ("Apple Watch Stand", "Technology", 22.50, 100),
-    ("Sunglasses", "Accessories", 12.50, 1000)    
-}
+    ("Sunglasses", "Accessories", 12.50, 1000);
+
+SELECT * FROM products;
+
+USE products; 
+DELETE FROM products WHERE item_id>=6;
+
+ALTER TABLE products ADD COLUMN inStock BOOLEAN DEFAULT True;
+
+
